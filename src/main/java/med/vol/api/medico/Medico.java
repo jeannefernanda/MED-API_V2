46b588ad818786fc0b262487c87c143ccb2508cf
Jeanne <jeannefernanda.m@gmail.com>
2024-11-08 19:45:29 -0400

    Criando as Interfaces Repository

--- src/main/java/med/vol/api/medico/Medico.java
+++ src/main/java/med/vol/api/medico/Medico.java
@@ -16,11 +16,19 @@ public class Medico {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    private String nome;
     private String email;
     private String crm;
     @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
     @Embedded
     private Endereco endereco;
+
+    public Medico(DadosCadastroMedico dados) {
+        this.nome = dados.nome();
+        this.crm = dados.crm();
+        this.email = dados.email();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
 }
