09cdb0944af8865084a0ca0aa83c4bd664ed2220
Jeanne <jeannefernanda.m@gmail.com>
2024-11-09 16:21:20 -0400

    Implementando o cadastro de paciente

--- src/main/java/med/vol/api/controller/PacienteController.java
+++ src/main/java/med/vol/api/controller/PacienteController.java
@@ -1,6 +1,12 @@
 package med.vol.api.controller;
 
+import jakarta.validation.Valid;
+import med.vol.api.medico.Medico;
 import med.vol.api.paciente.DadosCadastroPaciente;
+import med.vol.api.paciente.Paciente;
+import med.vol.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("pacientes")
 public class PacienteController {
+
+    @Autowired
+    private PacienteRepository repository;
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroPaciente dados){
-        System.out.println(dados);
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados){
+        repository.save(new Paciente(dados));
     }
 }
--- src/main/java/med/vol/api/paciente/DadosCadastroPaciente.java
+++ src/main/java/med/vol/api/paciente/DadosCadastroPaciente.java
@@ -1,6 +1,24 @@
 package med.vol.api.paciente;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.vol.api.endereco.DadosEndereco;
 
-public record DadosCadastroPaciente(String nome, String email, String Telefone, String CPF, DadosEndereco endereco) {
+public record DadosCadastroPaciente(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        String telefone,
+        @NotBlank
+        @Pattern(regexp = "\\d{11}")
+        String cpf,
+        @NotNull
+        @Valid
+        DadosEndereco endereco) {
 }
